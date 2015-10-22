ba4e35b57177e6d524e949e2559d898f779a4b02
Ryu Yu <ryuyu@microsoft.com>
2015-10-22 14:58:09 -0700

    updating bootstrapper for Windows PowerShell

--- Tasks/TacoCommand/bootstrapper.ps1
+++ Tasks/TacoCommand/bootstrapper.ps1
@@ -1,29 +1,37 @@
+#
+#  Copyright (c) Microsoft. All rights reserved.  
+#  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#
+
 param (
-    [string]$cwd,
-    [string]$msg
+    [string]$tacoCommand,
+    [string]$tacoArgs,
+    [string]$tacoVersion,
+    [string]$cwd
 )
 
-Write-Verbose 'Entering bootstrapper.ps1'
-Write-Verbose "cwd = $cwd"
-Write-Verbose "msg = $msg"
-
-# Import the Task.Common dll that has all the cmdlets we need for Build
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+ 
+$debug=Get-TaskVariable -Context $distributedTaskContext -Name "System.Debug"
+ 
+# Set env vars as expected by node script
+$Env:INPUT_TACOCOMMAND = $cordovaCommand
+$Env:INPUT_TACOARGS = $cordovaArgs
+$Env:INPUT_TACOVERSION = $cordovaVersion
+$Env:INPUT_CWD = $cwd
+$Env:SYSTEM_DEBUG = $debug
 
-if(!$cwd)
+# Node has an annoying habit of outputting warnings to standard error, so redirect stderr to stdout and call script
+$node = Get-Command -Name node -ErrorAction Ignore
+if(!$node)
 {
-    throw (Get-LocalizedString -Key "Working directory parameter is not set")
+    throw (Get-LocalizedString -Key "Unable to locate {0}" -ArgumentList 'node')
 }
 
-if(!(Test-Path $cwd -PathType Container))
-{
-    throw ("$cwd does not exist");
-}
-
-Write-Verbose "Setting working directory to $cwd"
-Set-Location $cwd
-
-Write-Host $msg
+#taco Script is in same spot as this powershell script
+$scriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+Invoke-Tool -Path $node.Path -Arguments "taco-command-task.js ##vso-task-powershell" -WorkingFolder $scriptRoot -WarningPattern "(^npm |^Note: |^Build option --|^WARN: )" 
 
 
 
