a9fe5c36176437df9ef400b5d1920f8ce2f7de0f
Ryu Yu <ryuyu@microsoft.com>
2015-10-27 13:24:55 -0700

    removed fallback version number from ionic command. TTB falls back to latest automatically now

--- Tasks/IonicCommand/ionic-command-task.js
+++ Tasks/IonicCommand/ionic-command-task.js
@@ -3,20 +3,20 @@
   Licensed under the MIT license. See LICENSE file in the project root for full license information.
 */
 
-var path = require('path'),
-	tl = require('./lib/vso-task-lib-proxy.js'),
-	ttb = require('taco-team-build');
+var path = require("path"),
+	tl = require("./lib/vso-task-lib-proxy.js"),
+	ttb = require("taco-team-build");
 
 
-var	buildSourceDirectory = tl.getVariable('build.sourceDirectory') || tl.getVariable('build.sourcesDirectory');
+var	buildSourceDirectory = tl.getVariable("build.sourceDirectory") || tl.getVariable("build.sourcesDirectory");
 //Process working directory
-var	cwd = tl.getInput('cwd') || buildSourceDirectory;
+var	cwd = tl.getInput("cwd") || buildSourceDirectory;
 tl.cd(cwd);
 
 callCordova()													
 	.fail(function(err) {
 		console.error(err.message);
-		tl.debug('taskRunner fail');
+		tl.debug("taskRunner fail");
 		tl.exit(1);
 	});
 
@@ -25,22 +25,22 @@ function callCordova(code) {
 	// Ionic requires the Cordova CLI in the path		
 	return ttb.cacheModule({
 			projectPath: cwd,
-			cordovaVersion : tl.getInput('cordovaVersion', false) 
+			cordovaVersion : tl.getInput("cordovaVersion", false) 
 		})
 		.then(function(result) {
 			// Add Cordova to path, then get Ionic
-			process.env.PATH = path.resolve(result.path, '..', '.bin') + path.delimiter + process.env.PATH;
+			process.env.PATH = path.resolve(result.path, "..", ".bin") + path.delimiter + process.env.PATH;
 			return 	ttb.cacheModule({
 				projectPath: cwd,
-				cordovaPackageName: 'ionic',
-				cordovaVersion : tl.getInput('ionicVersion', false) || '1.6.5'  //TODO: Attempt to use global if not specified
+				cordovaPackageName: "ionic",
+				cordovaVersion : tl.getInput("ionicVersion", false) || ""  //Empty string will attempt to use latest
 			});
 		})
 		.then(function(result) {
-			var ionicPath = process.platform == 'win32' ? 	path.resolve(result.path, '..', '.bin','ionic.cmd') : path.resolve(result.path, '..', '.bin','ionic')		
+			var ionicPath = process.platform == "win32" ? 	path.resolve(result.path, "..", ".bin","ionic.cmd") : path.resolve(result.path, "..", ".bin","ionic")		
 			var cdv = new tl.ToolRunner(ionicPath, true);
-			cdv.arg(tl.getDelimitedInput('ionicCommand', ' ', true));
-			var args = tl.getDelimitedInput('ionicArgs', ' ', false);
+			cdv.arg(tl.getDelimitedInput("ionicCommand", " ", true));
+			var args = tl.getDelimitedInput("ionicArgs", " ", false);
 			if(args) {
 				cdv.arg(args);			
 			}
