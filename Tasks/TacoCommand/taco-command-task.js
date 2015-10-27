c84213274154f4b462abae53ad5013884261ffa5
Ryu Yu <ryuyu@microsoft.com>
2015-10-27 13:42:31 -0700

    updating to latest taco-team-build and updating dependent tasks accordingly

--- Tasks/TacoCommand/taco-command-task.js
+++ Tasks/TacoCommand/taco-command-task.js
@@ -1,16 +1,16 @@
-var path = require('path');
-var tl = require('./lib/vso-task-lib-proxy.js');
-var ttb = require('taco-team-build');
+var path = require("path");
+var tl = require("./lib/vso-task-lib-proxy.js");
+var ttb = require("taco-team-build");
 
-var buildSourceDirectory = tl.getVariable('build.sourceDirectory') || tl.getVariable('build.sourcesDirectory');
+var buildSourceDirectory = tl.getVariable("build.sourceDirectory") || tl.getVariable("build.sourcesDirectory");
 //Process working directory
-var cwd = tl.getInput('cwd') || buildSourceDirectory;
+var cwd = tl.getInput("cwd") || buildSourceDirectory;
 tl.cd(cwd);
 
 callTaco()
     .fail(function (err) {
     console.error(err.message);
-    tl.debug('taskRunner fail');
+    tl.debug("taskRunner fail");
     tl.exit(1);
 });
 
@@ -18,23 +18,24 @@ function callTaco(code) {
     // taco requires the Cordova CLI in the path		
     return ttb.cacheModule({
         projectPath: cwd,
-        cordovaVersion: tl.getInput('cordovaVersion', false)
+        nodePackageName: "taco-cli",
+        moduleVersion: tl.getInput("tacoVersion", /*required*/ false)
     })
         .then(function (result) {
-        // Add Cordova to path, then get Ionic
-        process.env.PATH = path.resolve(result.path, '..', '.bin') + path.delimiter + process.env.PATH;
+        // Add TACO cli to PATH
+        process.env.PATH = path.resolve(result.path, "..", ".bin") + path.delimiter + process.env.PATH;
 
-        var tacoCmd = 'taco';
-        var tacoPath = tl.which(tacoCmd) || "./node_modules/taco-cli/bin/taco";
+        // and have the system resolve the location for us
+        var tacoPath = tl.which("taco");
         if (tacoPath) {
-            var cdv = new tl.ToolRunner(tacoPath, true);
-            cdv.arg(tl.getDelimitedInput('tacoCommand', ' ', true));
-            var args = tl.getDelimitedInput('tacoArgs', ' ', false);
+            var commandRunner = new tl.ToolRunner(tacoPath, true);
+            commandRunner.arg(tl.getDelimitedInput("tacoCommand", /*delim*/ " ", /*required*/ true));
+            var args = tl.getDelimitedInput("tacoArgs", /*delim*/ " ", /*required*/ false);
             if (args) {
-                cdv.arg(args);
+                commandRunner.arg(args);
             }
 
-            return cdv.exec();
+            return commandRunner.exec();
         }
     });
 }
