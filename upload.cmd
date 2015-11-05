4c6825230b239b0679a8859c832bb8eb00a4acbf
Chuck Lantz <clantz@microsoft.com>
2015-11-05 11:09:55 -0800

    Bug fixes for upload.cmd

--- upload.cmd
+++ upload.cmd
@@ -17,40 +17,40 @@ SET /p YN="Continue [Y/n]? "
 IF /I '%YN%'=='n' EXIT /B 1
 ECHO.
 
-CALL npm --version > NUL
-IF NOT ERRORLEVEL==0 GOTO INSTALLFAILED
+CALL npm --version 1>NUL 2>NUL
+IF NOT %ERRORLEVEL%==0 GOTO INSTALLFAILED
 
-CALL tfx --version > NUL
-IF NOT ERRORLEVEL==0 GOTO TFXINSTALL
+CALL tfx --version 1>NUL 2>NUL
+IF NOT %ERRORLEVEL%==0 GOTO TFXINSTALL
 
 :NPMINSTALL
 ECHO Installing dependencies...
 CALL npm install --only=prod
-IF NOT ERRORLEVEL==0 GOTO INSTALLFAILED
+IF NOT %ERRORLEVEL%==0 GOTO INSTALLFAILED
 
 :EXEC
 CALL node bin/tfxupload.js
-IF NOT ERRORLEVEL==0 GOTO UPLOADFAILED
+IF NOT %ERRORLEVEL%==0 GOTO UPLOADFAILED
 EXIT /B 0
 
 :TFXINSTALL
 ECHO Installing tfx-cli...
 CALL npm install -g tfx-cli
-IF NOT ERRORLEVEL==0 GOTO INSTALLFAILED
+IF NOT %ERRORLEVEL%==0 GOTO INSTALLFAILED
 ECHO Log in to the VSO/TFS collection you wish to deploy the tasks.
 CALL tfx login
-IF NOT ERRORLEVEL==0 GOTO LOGINFAILED
+IF NOT %ERRORLEVEL%==0 GOTO LOGINFAILED
 GOTO NPMINSTALL
 
 :UPLOADFAILED
 ECHO Failed to upload! Ensure Node.js is installed and in your path and you are logged into a VSO/TFS collection where you have build administration privileges.
-EXIT /B 1
+EXIT /B %ERRORLEVEL%
 
 :INSTALLFAILED
 ECHO Failed to install npm packages. Ensure Node.js is installed and node and npm are in your path.
-EXIT /B 1
+EXIT /B %ERRORLEVEL%
 
 :LOGINFAILED
 ECHO Login failed. Type "tfx login" to log in and then re-run this script.
-EXIT /B 1
+EXIT /B %ERRORLEVEL%
 
