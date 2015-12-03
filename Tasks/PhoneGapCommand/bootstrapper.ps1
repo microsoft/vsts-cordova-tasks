8f8a8ec3825b3232193c3dd7f74c9d5b2c988429
Ryu Yu <ryuyu@microsoft.com>
2015-12-03 15:10:19 -0800

    adding PhoneGap CLI Command task

---
+++ Tasks/PhoneGapCommand/bootstrapper.ps1
@@ -0,0 +1,34 @@
+#
+#  Copyright (c) Microsoft. All rights reserved.  
+#  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#
+
+param (
+    [string]$phonegapCommand,
+    [string]$phonegapArgs,
+    [string]$phonegapVersion,
+    [string]$cwd
+) 
+
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+ 
+$debug=Get-TaskVariable -Context $distributedTaskContext -Name "System.Debug"
+ 
+# Set env vars as expected by node script
+$Env:INPUT_PHONEGAPCOMMAND = $phonegapCommand
+$Env:INPUT_PHONEGAPARGS = $phonegapArgs
+$Env:INPUT_PHONEGAPVERSION = $phonegapVersion
+$Env:INPUT_CWD = $cwd
+$Env:SYSTEM_DEBUG = $debug
+
+# Node has an annoying habit of outputting warnings to standard error, so redirect stderr to stdout and call script
+$node = Get-Command -Name node -ErrorAction Ignore
+if(!$node)
+{
+    throw (Get-LocalizedString -Key "Unable to locate {0}" -ArgumentList 'node')
+}
+
+#phonegap Script is in same spot as this powershell script
+$scriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+Invoke-Tool -Path $node.Path -Arguments "phonegap-command-task.js ##vso-task-powershell" -WorkingFolder $scriptRoot 
---
+++ Tasks/PhoneGapCommand/phonegap-command-task.js
@@ -0,0 +1,50 @@
+/*
+  Copyright (c) Microsoft. All rights reserved.  
+  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+*/
+
+var path = require('path'),
+    taskLibrary = require('./lib/vso-task-lib-proxy.js'),
+    buildUtilities = require('taco-team-build');
+
+
+var buildSourceDirectory = taskLibrary.getVariable('build.sourceDirectory') || taskLibrary.getVariable('build.sourcesDirectory');
+//Process working directory
+var workingDirectory = taskLibrary.getInput('cwd', /*required*/ false) || buildSourceDirectory;
+taskLibrary.cd(workingDirectory);
+
+callCordova().fail(function (err) {
+    console.error(err.message);
+    taskLibrary.debug('taskRunner fail');
+    taskLibrary.exit(1);
+});
+
+// Main Cordova command exec
+function callCordova() {
+    var phonegapConfig = {
+        nodePackageName: 'phonegap',
+        projectPath: workingDirectory
+    };
+
+    var version = taskLibrary.getInput('phonegapVersion', /*required*/ false);
+    if (version) {
+        phonegapConfig.moduleVersion = version;
+    }
+
+    return buildUtilities.cacheModule(phonegapConfig).then(function (phonegapModule) {
+        taskLibrary.debug('PhoneGap Module Path: ' + phonegapModule.path);
+         
+        var phonegapExecutable = process.platform == 'win32' ? 'phonegap.cmd' : 'phonegap';
+        var phonegapCmd = path.resolve(phonegapModule.path, '..', '.bin', phonegapExecutable);
+        var commandRunner = new taskLibrary.ToolRunner(phonegapCmd);
+
+        commandRunner.arg(taskLibrary.getDelimitedInput('phonegapCommand', /*delim*/ ' ', /*required*/ true));
+        var args = taskLibrary.getDelimitedInput('phonegapArgs', /*delim*/ ' ', /*required*/ false);
+        if (args) {
+            commandRunner.arg(args);
+        }
+
+        return commandRunner.exec();
+    });
+}
+
