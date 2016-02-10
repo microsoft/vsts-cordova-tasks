408952e399c652cd7afe2cbc3c88501898714b7d
Bret Johnson <bret.johnson@microsoft.com>
2016-02-10 16:35:56 -0500

    Updated makevsix, with changes suggested by Peter & changing usage (also uses command-line-args now instad of yargs)

--- makevsix.js
+++ makevsix.js
@@ -7,16 +7,25 @@ require('shelljs/global');
 
 var path = require('path'),
     fs = require('fs');
+var commandLineArgs = require('command-line-args');
 
-var argv = require('yargs')
-    .usage('Usage: $0 <command>')
-    .command('create', 'Create VSIX')
-    .command('publishtest', 'Publish test VSIX')
-    .demand(1)
-    .argv;
-	
+var cli = commandLineArgs([
+  { name: 'maketest', alias: 't', description: 'make test VSIX', type: Boolean },
+  { name: 'makeprod', alias: 'p', description: 'make production VSIX', type: Boolean },
+  { name: 'publishtest', alias: 'b', description: 'publish test VSIX (implies --maketest)', type: Boolean },
+]);
 
-var command = argv._[0];
+var options;
+try {
+    options = cli.parse()
+}
+catch(err) {
+    echo(err);
+    invalidUsage();
+}
+
+if (Object.keys(options).length === 0)
+    invalidUsage();
 
 if (! exec("npm --version", {silent: true})) {
     echo("npm not found. please install npm and run again.")
@@ -26,23 +35,51 @@ if (! exec("npm --version", {silent: true})) {
 if (exec('tfx --version', {silent: true}).code != 0)
     echoAndExec("tfx-cli not found. installing", "npm install -g tfx-cli");
 
-installTasks()
+//installTasks()
 
-if (command === 'create')
-    echoAndExec('Creating VSIX', 'tfx extension create --manifest-globs mobiledevopscordovaextension.json --override {\\\"public\\\":true}');
-else if (command === 'publishtest') {
-    var token = env['PUBLISH_ACCESSTOKEN'];
-    if (token === undefined) {
+if (options.makeprod) {
+    var override = {
+        public: true
+    }
+
+    echoAndExec('Creating VSIX', 'tfx extension create --manifest-globs mobiledevopscordovaextension.json --override ' + toOverrideString(override));
+}
+
+var testOverride = {
+    public: false,
+    name: "Cordova Build-Dev",
+    id: "cordova-extension-dev",
+    publisher: "ms-mobiledevops-test"
+}
+
+if (options.maketest)
+    echoAndExec('Creating test VSIX',
+        'tfx extension create --manifest-globs mobiledevopscordovaextension.json --override ' + toOverrideString(testOverride));
+
+if (options.publishtest) {
+    var accessToken = env['PUBLISH_ACCESSTOKEN'];
+    if (accessToken === undefined) {
         echo("Must set PUBLISH_ACCESSTOKEN environment variable to publish a test VSIX");
         exit(1);
     }
-    
-    echoAndExec('Creating and publishing test VSIX...',
-        'tfx extension publish --manifest-globs mobiledevopscordovaextension.json --override { \\\"public\\\": false, \\\"name\\\": \\\"Cordova Build-Dev\\\", \\\"id\\\": \\\"cordova-extension-dev\\\", \\\"publisher\\\": \\\"ms-mobiledevops-test\\\"} --share-with mobiledevops x04ty29er --token ' + env['PUBLISH_ACCESSTOKEN']);
+
+    echoAndExec('Publishing test VSIX',
+        'tfx extension publish --manifest-globs mobiledevopscordovaextension.json --override ' + toOverrideString(testOverride) + ' --share-with mobiledevops x04ty29er --token ' + accessToken);
 }
-else {
-    echo("Invalid command: " + command);
-    exit(1);    
+
+//
+// Functions
+//
+
+function invalidUsage() {
+   echo("Usage: makevsix [options]; must supply at least one option")
+   echo(cli.getUsage());
+   exit(1);
+}
+
+// Convert the override JavaScript object to a JSON string, adding a backslash before any double quotes as that's needed for the shell exec   
+function toOverrideString(object) {
+    return JSON.stringify(object).replace(/"/g, '\\"');
 }
 
 function installTasks() {
@@ -58,7 +95,7 @@ function installTasks() {
 
         console.log('Installing npm dependencies for task...');
         if (exec('npm install --only=prod').code != 0) {
-            console.log("npm install for task failed");
+            console.log('npm install for task ' + task + ' failed');
             exit(1);
         }
     });
